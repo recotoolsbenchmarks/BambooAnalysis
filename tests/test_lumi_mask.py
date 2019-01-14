8423368955e3795fac23c022ab89e06d79c54786
Pieter David <pieter.david@uclouvain.be>
2019-01-14 12:00:20 +0100

    Lumi mask test: local file, split cases

--- tests/test_lumi_mask.py
+++ tests/test_lumi_mask.py
@@ -1,22 +1,25 @@
-import os.path
-from urllib.parse import urlparse
-import subprocess
-jsonPath = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt"
-runRange = (276831, 277420) ## 2016E 
-jsonName = urlparse(jsonPath).path.split("/")[-1]
-if not os.path.exists(jsonName):
-    print("Downloading JSON file from {0}".format(jsonPath))
-    subprocess.check_call(["wget", jsonPath])
-import bamboo.treefunctions ## loads and includes
-from cppyy import gbl
-#ROOT.gSystem.Load("libBambooLumiMask.so")
-#ROOT.gROOT.ProcessLine('#include "../ext/include/LumiMask.h"')
-lm = gbl.LumiMask.fromJSON(jsonName, *runRange)
+import pytest
 
-def test_lumimask():
-    assert not lm.accept(283681, 10 ) ## excluded run
-    assert     lm.accept(276948, 10 ) ## included run
-    assert     lm.accept(276948, 1  ) ## first for a run
-    assert     lm.accept(276834, 720) ## last for that run
-    assert not lm.accept(277070, 310) ## excluded part
-    assert     lm.accept(277070, 311) ## later block
+@pytest.fixture(scope="module")
+def json_lumimask():
+    import os.path
+    jsonName = os.path.join(os.path.dirname(__file__), "Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt")
+    runRange = (276831, 277420) ## 2016E
+    import bamboo.treefunctions ## loads and includes, minimal alternative below
+    from cppyy import gbl
+    #ROOT.gSystem.Load("libBambooLumiMask.so")
+    #ROOT.gROOT.ProcessLine('#include "../ext/include/LumiMask.h"')
+    yield gbl.LumiMask.fromJSON(jsonName, *runRange)
+
+def test_lumimask_excluded(json_lumimask):
+    assert not json_lumimask.accept(283681, 10 ) ## excluded run
+def test_lumimask_included(json_lumimask):
+    assert     json_lumimask.accept(276948, 10 ) ## included run
+def test_lumimask_first(json_lumimask):
+    assert     json_lumimask.accept(276948, 1  ) ## first for a run
+def test_lumimask_last(json_lumimask):
+    assert     json_lumimask.accept(276834, 720) ## last for that run
+def test_lumimask_later_excluded(json_lumimask):
+    assert not json_lumimask.accept(277070, 310) ## excluded part
+def test_lumimask_later_included(json_lumimask):
+    assert     json_lumimask.accept(277070, 311) ## later block

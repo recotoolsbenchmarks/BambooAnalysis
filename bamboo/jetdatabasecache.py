e9d6aa600132c9a31330f71e05c13855971c729c
Pieter David <pieter.david@cern.ch>
2019-07-09 11:46:08 +0200

    Jet database cache: lock for writing (fine-grained, such that reading can be done in parallel)

--- bamboo/jetdatabasecache.py
+++ bamboo/jetdatabasecache.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import base64
 import json
 import logging
@@ -7,19 +8,54 @@ import os.path
 import requests
 
 class JetDatabaseCache(object):
-    def __init__(self, name, service="https://api.github.com/repos", repository=None, branch="master", cachedir=None):
+    def __init__(self, name, service="https://api.github.com/repos", repository=None, branch="master", cachedir=None, mayWrite=True):
+        self._mayWrite = mayWrite
         if cachedir is None:
             from .analysisutils import bamboo_cachedir
             cachedir = bamboo_cachedir
         self.cachedir = os.path.join(cachedir, name)
-        if not os.path.isdir(self.cachedir):
-            os.makedirs(self.cachedir)
         self.statusFile = os.path.join(self.cachedir, "status.json")
+        self._statusLockName = "{0}.lock".format(self.statusFile)
         self.service = service
         self.repository = repository
         self.branch = branch
         self._status = None
         self._baseUrl = None
+        self._init()
+
+    @contextmanager
+    def _statusLockAndSave(self, expires=None):
+        if not self._mayWrite:
+            raise RuntimeError("Tried to update jet database cache from read-only handle (worker task), please make sure all payloads are there by running locally, e.g. with --maxFiles=1")
+        import time
+        from datetime import datetime
+        while True:
+            if not os.path.exists(self._statusLockName):
+                if not os.path.isdir(self.cachedir):
+                    os.makedirs(self.cachedir, exist_ok=True)
+                mypid = "{0}:{1:d}\n".format(os.uname()[1], os.getpid())
+                try:
+                    with open(self._statusLockName, "x") as lf:
+                        lf.write(mypid)
+                except OSError as ex:
+                    logger.debug("Failed to acquire lock ({0}), waiting a bit to try again".format(str(ex)))
+                except FileExistsError as ex:
+                    logger.debug("Failed to acquire lock ({0}), waiting a bit to try again".format(str(ex)))
+                else:
+                    logger.debug("Acquired write lock for {0}".format(self.statusFile))
+                    yield
+                    with open(self.statusFile, "w") as sFile:
+                        json.dump(self._status, sFile)
+                    os.remove(self._statusLockName)
+                    logger.debug("Released write lock for {0}".format(self.statusFile))
+                    break
+                time.sleep(5)
+            elif expires is not None and ( ( datetime.now() - datetime.fromtimestamp(os.path.getctime(self._statusLockName)) ) > expires ):
+                logger.warning("Lock file expired, removing")
+                os.remove(self._statusLockName)
+            else:
+                logger.debug("{0} locked, waiting another 5s".format(self.statusFile))
+                time.sleep(5) # wait my turn
 
     def _get_master_sha(self):
         headers = {}
@@ -33,7 +69,7 @@ class JetDatabaseCache(object):
             self._status["branches_etag"] = r_branches.headers["ETag"]
             return r_master["object"]["sha"]
 
-    def __enter__(self):
+    def _init(self):
         if os.path.exists(self.statusFile):
             with open(self.statusFile) as sFile:
                 self._status = json.load(sFile)
@@ -58,40 +94,41 @@ class JetDatabaseCache(object):
         ## Update the *tree* of the (fetched tags of) textFiles if necessary
         if "sha" not in self._status or self._status["sha"] != masterSHA:
             logger.debug("Updating root of {0} at {1}".format(self.repository, masterSHA))
-            r_rootTree = requests.get("{0}/git/trees/{1}".format(self._baseUrl, masterSHA)).json()
-            r_TF = next(itm for itm in r_rootTree["tree"] if itm["path"] == "textFiles")
-            if "textFiles" not in self._status:
-                self._status["textFiles"] = {"tree":{}}
-            statTF = self._status["textFiles"]
-            if statTF.get("sha") != r_TF["sha"]:
-                stTags = statTF["tree"]
-                logger.debug("Updating 'textFiles' of {0} at {1} ({2})".format(self.repository, masterSHA, r_TF["sha"]))
-                r_tagsTree = requests.get("{0}/git/trees/{1}".format(self._baseUrl, r_TF["sha"])).json()
-                for r_tg in r_tagsTree["tree"]:
-                    if r_tg["path"] not in stTags:
-                        logger.debug("New tag in {0}: {1}".format(self.repository, r_tg["path"]))
-                        stTags[r_tg["path"]] = {"sha": r_tg["sha"]}
-                    else:
-                        statTag = stTags[r_tg["path"]]
-                        if r_tg["sha"] != statTag["sha"]:
-                            if "tree" in statTag:
-                                self._updateTag(r_tg["path"], r_tg["sha"])
-                            else: ## don't trigger fetch
-                                statTag["sha"] = r_tg["sha"]
-                toRemove = []
-                for tagName, statTag in stTags.items():
-                    if not any(r_tg["path"] == tagName for r_tg in r_tagsTree["tree"]):
-                        logger.debug("Tag {0} was removed from {1}".format(tagName, self.repository))
-                        tagDir = os.path.join(self.cachedir, tagName)
-                        if os.path.isdir(tagDir):
-                            import shutil
-                            logger.debug("Should remove cache directory {0}".format(tagDir))
-                            #shutil.rmtree(tagDir)
-                        toRemove.append(tagName)
-                for tagName in toRemove:
-                    del stTags[tagName]
-                statTF["sha"] = r_TF["sha"]
-            self._status["sha"] = masterSHA
+            with self._statusLockAndSave():
+                r_rootTree = requests.get("{0}/git/trees/{1}".format(self._baseUrl, masterSHA)).json()
+                r_TF = next(itm for itm in r_rootTree["tree"] if itm["path"] == "textFiles")
+                if "textFiles" not in self._status:
+                    self._status["textFiles"] = {"tree":{}}
+                statTF = self._status["textFiles"]
+                if statTF.get("sha") != r_TF["sha"]:
+                    stTags = statTF["tree"]
+                    logger.debug("Updating 'textFiles' of {0} at {1} ({2})".format(self.repository, masterSHA, r_TF["sha"]))
+                    r_tagsTree = requests.get("{0}/git/trees/{1}".format(self._baseUrl, r_TF["sha"])).json()
+                    for r_tg in r_tagsTree["tree"]:
+                        if r_tg["path"] not in stTags:
+                            logger.debug("New tag in {0}: {1}".format(self.repository, r_tg["path"]))
+                            stTags[r_tg["path"]] = {"sha": r_tg["sha"]}
+                        else:
+                            statTag = stTags[r_tg["path"]]
+                            if r_tg["sha"] != statTag["sha"]:
+                                if "tree" in statTag:
+                                    self._updateTag(r_tg["path"], r_tg["sha"])
+                                else: ## don't trigger fetch
+                                    statTag["sha"] = r_tg["sha"]
+                    toRemove = []
+                    for tagName, statTag in stTags.items():
+                        if not any(r_tg["path"] == tagName for r_tg in r_tagsTree["tree"]):
+                            logger.debug("Tag {0} was removed from {1}".format(tagName, self.repository))
+                            tagDir = os.path.join(self.cachedir, tagName)
+                            if os.path.isdir(tagDir):
+                                import shutil
+                                logger.debug("Should remove cache directory {0}".format(tagDir))
+                                #shutil.rmtree(tagDir)
+                            toRemove.append(tagName)
+                    for tagName in toRemove:
+                        del stTags[tagName]
+                    statTF["sha"] = r_TF["sha"]
+                self._status["sha"] = masterSHA
         else:
             logger.debug("{0} tree up to date at {1}".format(self.repository, self._status["sha"]))
 
@@ -137,41 +174,39 @@ class JetDatabaseCache(object):
             raise ValueError("Unknown tag: {0}".format(tag))
         stTag = stTags[tag]
         if "tree" not in stTag:
-            self._updateTag(tag, stTag["sha"])
+            with self._statusLockAndSave():
+                self._updateTag(tag, stTag["sha"])
         if fName not in stTag["tree"]:
             raise ValueError("Unknown file: {0}/{1}".format(tag, fName))
         stPL = stTag["tree"][fName]
         if len(stPL) == 1:
             logger.debug("Getting payload for {0}/{1} ({2})".format(tag, fName, stPL["sha"]))
-            r_pl = requests.get("{0}/git/blobs/{1}".format(self._baseUrl, stPL["sha"])).json()
-            if r_pl["encoding"] == "base64":
-                content = base64.b64decode(r_pl["content"])
-            else:
-                raise RuntimeError("Unknown encoding: {0}".format(r_pl["encoding"])) 
-            if content.startswith(b"../") and len(content.split(b"\n")) == 1:
-                _,actTag,actFName = content.decode().strip().split(os.sep)
-                stPL["symlink"] = (actTag, actFName)
-            else:
-                tagDir = os.path.join(self.cachedir, tag)
-                if not os.path.exists(tagDir):
-                    os.makedirs(tagDir)
-                plFName = os.path.join(tagDir, fName)
-                with open(plFName, "wb") as plF:
-                    plF.write(content)
-                stPL["path"] = plFName
-                logger.debug("Saved as {0}".format(plFName))
+            with self._statusLockAndSave():
+                r_pl = requests.get("{0}/git/blobs/{1}".format(self._baseUrl, stPL["sha"])).json()
+                if r_pl["encoding"] == "base64":
+                    content = base64.b64decode(r_pl["content"])
+                else:
+                    raise RuntimeError("Unknown encoding: {0}".format(r_pl["encoding"])) 
+                if content.startswith(b"../") and len(content.split(b"\n")) == 1:
+                    _,actTag,actFName = content.decode().strip().split(os.sep)
+                    stPL["symlink"] = (actTag, actFName)
+                else:
+                    tagDir = os.path.join(self.cachedir, tag)
+                    if not os.path.exists(tagDir):
+                        os.makedirs(tagDir)
+                    plFName = os.path.join(tagDir, fName)
+                    with open(plFName, "wb") as plF:
+                        plF.write(content)
+                    stPL["path"] = plFName
+                    logger.debug("Saved as {0}".format(plFName))
         if "symlink" in stPL:
             logger.debug("Payload {0}/{1} is symlinked to {2}/{3}".format(tag, fName, *stPL["symlink"]))
             return self._getPayload(*stPL["symlink"])
         elif "path" in stPL:
             return stPL["path"]
 
-    def __exit__(self, type, value, traceback):
-        with open(self.statusFile, "w") as sFile:
-            json.dump(self._status, sFile)
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     import IPython
-    with JetDatabaseCache("JECDB", repository="cms-jet/JECDatabase", cachedir=".") as jecDBCache:
-        IPython.embed()
+    jecdb = JetDatabaseCache("JECDB", repository="cms-jet/JECDatabase", cachedir=".")
+    IPython.embed()

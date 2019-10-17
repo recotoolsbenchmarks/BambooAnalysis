cf56d7d1d248289bf6db5613049ae92a6b782a92
Pieter David <pieter.david@cern.ch>
2019-10-17 17:30:30 +0200

    Some refactoring of the getjetclasses script

--- ext/getjetclasses.sh
+++ ext/getjetclasses.sh
@@ -1,28 +1,80 @@
 #!/bin/bash
 CMSSW_REL_BASE="/cvmfs/cms.cern.ch/slc7_amd64_gcc700/cms/cmssw/CMSSW_10_5_0_pre2"
-JMOPKG="${CMSSW_REL_BASE}/src/CondFormats/JetMETObjects"
+
 DEST_INC="CMSJet/include"
 DEST_SRC="CMSJet/src"
 mkdir -p "${DEST_INC}"
 mkdir -p "${DEST_SRC}"
+if [ -d "/cvmfs/cms.cern.ch" ]
+then
+  function get_cms {
+    cp "${CMSSW_REL_BASE}/src/${1}" "${2}"
+  }
+else
+  echo "Should download from github now..."
+  exit 0
+fi
+anyWasModified=""
 
-cp "${JMOPKG}/interface/Utilities.h" "${DEST_INC}"
+function getheader {
+  local pkg="${1}"
+  local cls="${2}"
+  if [ ! -e "${DEST_INC}/${cls}.h" ]; then
+    get_cms "${pkg}/interface/${cls}.h" "${DEST_INC}"
+    anyWasModified="yes"
+  fi
+}
+function getheadernp {
+  local pkg="${1}"
+  local cls="${2}"
+  if [ ! -e "${DEST_INC}/${cls}.h" ]; then
+    get_cms "${pkg}/src/${cls}.h" "${DEST_INC}"
+    anyWasModified="yes"
+  fi
+}
+function getimplem {
+  local pkg="${1}"
+  local cls="${2}"
+  if [ ! -e "${DEST_SRC}/${cls}.cc" ]; then
+    get_cms "${pkg}/src/${cls}.cc" "${DEST_SRC}"
+    anyWasModified="yes"
+  fi
+}
+function getclass {
+  local pkg="${1}"
+  local cls="${2}"
+  getheader "${pkg}" "${cls}"
+  getimplem "${pkg}" "${cls}"
+}
+function getclassnp {
+  local pkg="${1}"
+  local cls="${2}"
+  getheadernp "${pkg}" "${cls}"
+  getimplem "${pkg}" "${cls}"
+}
+
+## JEC/JER-related classes
+getheader "CondFormats/JetMETObjects" "Utilities"
 jmoClassNames=(JetCorrectionUncertainty JetCorrectorParameters JetCorrectorParametersHelper JetResolutionObject SimpleJetCorrectionUncertainty SimpleJetCorrector FactorizedJetCorrector FactorizedJetCorrectorCalculator)
 for className in "${jmoClassNames[@]}"
 do
-  cp "${JMOPKG}/interface/${className}.h" "${DEST_INC}"
-  cp "${JMOPKG}/src/${className}.cc" "${DEST_SRC}"
+  getclass "CondFormats/JetMETObjects" "${className}"
 done
-JMMPKG="${CMSSW_REL_BASE}/src/JetMETCorrections/Modules"
-cp "${JMMPKG}/interface/JetResolution.h" "${DEST_INC}"
-cp "${JMMPKG}/src/JetResolution.cc" "${DEST_SRC}"
-CMUPKG="${CMSSW_REL_BASE}/src/CommonTools/Utils"
-cp "${CMUPKG}/interface/FormulaEvaluator.h" "${DEST_INC}"
-cp "${CMUPKG}/src/FormulaEvaluator.cc" "${DEST_SRC}"
-for ffn in $(find "${CMUPKG}/src" -type f -name "formula*")
-do
-  cp "${ffn}" "${DEST_SRC}"
-done
-pushd CMSJet
-patch -p2 -i "$(dirname $0)/jetclasses.patch"
-popd
+getclass "JetMETCorrections/Modules" "JetResolution"
+## formula evaluator
+getclass "CommonTools/Utils" "FormulaEvaluator"
+getheadernp "CommonTools/Utils" "formulaBinaryOperatorEvaluator"
+getheadernp "CommonTools/Utils" "formulaFunctionOneArgEvaluator"
+getheadernp "CommonTools/Utils" "formulaFunctionTwoArgsEvaluator"
+getheadernp "CommonTools/Utils" "formulaUnaryMinusEvaluator"
+getclassnp "CommonTools/Utils" "formulaConstantEvaluator"
+getclassnp "CommonTools/Utils" "formulaEvaluatorBase"
+getclassnp "CommonTools/Utils" "formulaParameterEvaluator"
+getclassnp "CommonTools/Utils" "formulaVariableEvaluator"
+
+## now patch them
+if [ "${anyWasModified}" != "" ]; then
+  pushd CMSJet > /dev/null
+  patch -p2 -i "$(dirname $0)/jetclasses.patch" > /dev/null
+  popd > /dev/null
+fi

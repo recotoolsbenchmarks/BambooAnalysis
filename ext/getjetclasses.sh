7a34a80f07d919210d98e0c81428a530f803d83c
Pieter David <pieter.david@cern.ch>
2019-10-17 13:41:56 +0200

    Get jet classes in the build tree, automatically

--- ext/getjetclasses.sh
+++ ext/getjetclasses.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 CMSSW_REL_BASE="/cvmfs/cms.cern.ch/slc7_amd64_gcc700/cms/cmssw/CMSSW_10_5_0_pre2"
 JMOPKG="${CMSSW_REL_BASE}/src/CondFormats/JetMETObjects"
-cp "${JMOPKG}/interface/Utilities.h" include/
+DEST_INC="CMSJet/include"
+DEST_SRC="CMSJet/src"
+mkdir -p "${DEST_INC}"
+mkdir -p "${DEST_SRC}"
+
+cp "${JMOPKG}/interface/Utilities.h" "${DEST_INC}"
 jmoClassNames=(JetCorrectionUncertainty JetCorrectorParameters JetCorrectorParametersHelper JetResolutionObject SimpleJetCorrectionUncertainty SimpleJetCorrector FactorizedJetCorrector FactorizedJetCorrectorCalculator)
 for className in "${jmoClassNames[@]}"
 do
-  cp "${JMOPKG}/interface/${className}.h" include/
-  cp "${JMOPKG}/src/${className}.cc" src/
+  cp "${JMOPKG}/interface/${className}.h" "${DEST_INC}"
+  cp "${JMOPKG}/src/${className}.cc" "${DEST_SRC}"
 done
 JMMPKG="${CMSSW_REL_BASE}/src/JetMETCorrections/Modules"
-cp "${JMMPKG}/interface/JetResolution.h" include/
-cp "${JMMPKG}/src/JetResolution.cc" src/
+cp "${JMMPKG}/interface/JetResolution.h" "${DEST_INC}"
+cp "${JMMPKG}/src/JetResolution.cc" "${DEST_SRC}"
 CMUPKG="${CMSSW_REL_BASE}/src/CommonTools/Utils"
-cp "${CMUPKG}/interface/FormulaEvaluator.h" include/
-cp "${CMUPKG}/src/FormulaEvaluator.cc" src/
+cp "${CMUPKG}/interface/FormulaEvaluator.h" "${DEST_INC}"
+cp "${CMUPKG}/src/FormulaEvaluator.cc" "${DEST_SRC}"
 for ffn in $(find "${CMUPKG}/src" -type f -name "formula*")
 do
-  cp "${ffn}" src/
+  cp "${ffn}" "${DEST_SRC}"
 done
-patch -p2 -i jetclasses.patch
+pushd CMSJet
+patch -p2 -i "$(dirname $0)/jetclasses.patch"
+popd

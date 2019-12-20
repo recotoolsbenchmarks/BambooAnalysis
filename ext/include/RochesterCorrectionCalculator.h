d13687fa6988432f68dc438e5d65245f668079d2
Pieter David <pieter.david@cern.ch>
2019-12-20 11:46:19 +0100

    SOA for kinematic variations, no sorting; port Rochester correction calculator

--- ext/include/RochesterCorrectionCalculator.h
+++ ext/include/RochesterCorrectionCalculator.h
@@ -9,10 +9,9 @@ class RoccoR;
 
 class RochesterCorrectionCalculator {
 public:
-  using result_entry_t = rdfhelpers::ModifiedKinCollection;
-  using result_t = boost::container::flat_map<std::string,result_entry_t>;
-  using p4compv_t = ROOT::VecOps::RVec<float>;
-  using LorentzVector = result_entry_t::LorentzVector;
+  using result_t = rdfhelpers::ModifiedPtCollection;
+  using LorentzVector = result_t::LorentzVector;
+  using p4compv_t = result_t::p4compv_t;
 
   RochesterCorrectionCalculator() {}
 
@@ -26,18 +25,8 @@ public:
   result_t produceModifiedCollections(
       const p4compv_t& muon_pt, const p4compv_t& muon_eta, const p4compv_t& muon_phi, const p4compv_t& muon_mass,
       const ROOT::VecOps::RVec<Int_t>& muon_charge, const ROOT::VecOps::RVec<Int_t>& muon_nlayers, const ROOT::VecOps::RVec<Int_t>& muon_genIdx, const p4compv_t& gen_pt) const;
-
-  struct Muon {
-    std::size_t i;
-    LorentzVector p4;
-    Muon(std::size_t i_, const LorentzVector& p4_) : i(i_), p4(p4_) {}
-    Muon(std::size_t i_, float pt, float eta, float phi, float m) : i(i_), p4(pt, eta, phi, m) {}
-  };
-
 private:
   mutable std::mt19937 m_random; // for resolution
   struct roccordeleter { void operator()(RoccoR*) const; };
   std::unique_ptr<RoccoR,roccordeleter> m_roccor;
-
-  result_t produceModifiedCollections(std::vector<Muon>& muons, const ROOT::VecOps::RVec<Int_t>& muon_charge, const ROOT::VecOps::RVec<Int_t>& muon_nlayers, const p4compv_t& muon_genpt) const;
 };
--- ext/include/kinematicvariations.h
+++ ext/include/kinematicvariations.h
@@ -3,6 +3,7 @@
 #include <ROOT/RVec.hxx>
 #include <Math/GenVector/LorentzVector.h>
 #include <Math/GenVector/PtEtaPhiM4D.h>
+#include <boost/container/flat_map.hpp>
 
 namespace rdfhelpers {
 
@@ -23,5 +24,74 @@ private:
   Indices m_indices;
   Momenta m_momenta;
 }; // TODO make a jet-specific version with MET
+
+class ModifiedPtCollection { // map of variation collections
+public:
+  using LorentzVector = ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float>>;
+  using p4compv_t = ROOT::VecOps::RVec<float>;
+
+  ModifiedPtCollection(p4compv_t eta, p4compv_t phi, p4compv_t mass) : m_eta(eta), m_phi(phi), m_mass(mass) {}
+  void add(const std::string& name, p4compv_t pt)
+  {
+    m_variations[name] = pt;
+  }
+
+  const p4compv_t& eta() const { return m_eta; }
+  const p4compv_t& phi() const { return m_phi; }
+  const p4compv_t& mass() const { return m_mass; }
+
+  class Variation { // single variation
+  public:
+    Variation(const ModifiedPtCollection& parent, const p4compv_t& pt) : m_parent(parent), m_pt(pt) {}
+    const p4compv_t& pt  () const { return m_pt; }
+    const p4compv_t& eta () const { return m_parent.eta(); }
+    const p4compv_t& phi () const { return m_parent.phi(); }
+    const p4compv_t& mass() const { return m_parent.mass(); }
+  private:
+    const ModifiedPtCollection& m_parent;
+    const p4compv_t& m_pt;
+  };
+
+  Variation operator[] (const std::string& name) const { return Variation(*this, m_variations.at(name)); }
+private:
+  boost::container::flat_map<std::string,p4compv_t> m_variations;
+  p4compv_t m_eta, m_phi, m_mass;
+};
+
+class ModifiedPtMCollection { // map of variation collections
+public:
+  using LorentzVector = ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float>>;
+  using p4compv_t = ROOT::VecOps::RVec<float>;
+
+  ModifiedPtMCollection(p4compv_t eta, p4compv_t phi) : m_eta(eta), m_phi(phi) {}
+  void add(const std::string& name, p4compv_t pt, p4compv_t mass)
+  {
+    m_variations[name] = std::make_pair(pt, mass);
+  }
+
+  const p4compv_t& eta() const { return m_eta; }
+  const p4compv_t& phi() const { return m_phi; }
+
+  class Variation { // single variation
+  public:
+    Variation(const ModifiedPtMCollection& parent, const p4compv_t& pt, const p4compv_t& mass) : m_parent(parent), m_pt(pt), m_mass(mass) {}
+    const p4compv_t& pt  () const { return m_pt; }
+    const p4compv_t& eta () const { return m_parent.eta(); }
+    const p4compv_t& phi () const { return m_parent.phi(); }
+    const p4compv_t& mass() const { return m_mass; }
+  private:
+    const ModifiedPtMCollection& m_parent;
+    const p4compv_t& m_pt;
+    const p4compv_t& m_mass;
+  };
+
+  Variation operator[] (const std::string& name) const {
+    const auto& varPM = m_variations.at(name);
+    return Variation(*this, varPM.first, varPM.second);
+  }
+private:
+  boost::container::flat_map<std::string,std::pair<p4compv_t,p4compv_t>> m_variations;
+  p4compv_t m_eta, m_phi;
+};
 
 };

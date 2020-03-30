ed879055b0cba4a16e3f659ff4ed1a0e59a1eea8
Pieter David <pieter.david@cern.ch>
2020-03-30 18:39:18 +0200

    Add tests for postprocessed data NanoAOD

--- tests/test_plots_postprocnano.py
+++ tests/test_plots_postprocnano.py
@@ -1,24 +1,39 @@
 import pytest
 
+import logging
+logger = logging.getLogger(__name__)
+
 import os.path
 testData = os.path.join(os.path.dirname(__file__), "data")
 
 @pytest.fixture(scope="module")
-def decoNano():
+def decoNanoMC16():
     from bamboo import treefunctions as op
     from bamboo.root import gbl
     f = gbl.TFile.Open(os.path.join(testData, "DY_M50_2016postproc_JMEKin_bTagShape_puWeight.root"))
     tree = f.Get("Events")
-    from bamboo.treedecorators import decorateNanoAOD, NanoAODDescription, nanoPUWeightVar, nanoReadJetMETVar
+    from bamboo.treedecorators import decorateNanoAOD, NanoAODDescription, nanoPUWeightVar, ReadJetMETVar
     from bamboo.dataframebackend import DataframeBackend
-    tup = decorateNanoAOD(tree, NanoAODDescription.get("v5", year="2016", isMC=True, systVariations=[nanoPUWeightVar, nanoReadJetMETVar]))
+    nanoReadJetMETVar_MC = ReadJetMETVar("Jet", "MET", jetsExclVars=["raw", "jesHF", "jesHF_2018", "jesHF_2017", "jesHF_2016"], metNomName="jer", metExclVars=["raw", "nom"], bTaggers=["csvv2", "deepcsv", "deepjet", "cmva"], bTagWPs=["L", "M", "T", "shape"])
+    tup = decorateNanoAOD(tree, NanoAODDescription.get("v5", year="2016", isMC=True, systVariations=[nanoPUWeightVar, nanoReadJetMETVar_MC]))
     be, noSel = DataframeBackend.create(tup)
     yield tup, noSel, be
 
-def test(decoNano):
+@pytest.fixture(scope="module")
+def decoNanoData16():
+    from bamboo import treefunctions as op
+    from bamboo.root import gbl
+    f = gbl.TFile.Open(os.path.join(testData, "DoubleMuon_2016Gpostproc_JMEKin.root"))
+    tree = f.Get("Events")
+    from bamboo.treedecorators import decorateNanoAOD, NanoAODDescription, ReadJetMETVar
+    from bamboo.dataframebackend import DataframeBackend
+    tup = decorateNanoAOD(tree, NanoAODDescription.get("v5", year="2016", isMC=False, systVariations=[
+        ReadJetMETVar("Jet", "MET", jetsNomName="nom", metNomName="nom")]))
+    be, noSel = DataframeBackend.create(tup)
+    yield tup, noSel, be
+
+def definePlots(tup, noSel, isMC=False):
     # a somewhat realistic (but not very sensible) combination of selections and plots
-    tup, noSel, be = decoNano
-    noSel = noSel.refine("mcWeight", weight=[ tup.genWeight, tup.puWeight ])
     from bamboo import treefunctions as op
     from bamboo.plots import Plot, Selection, EquidistantBinning
     plots = []
@@ -30,15 +45,36 @@ def test(decoNano):
     jets = op.select(tup.Jet, lambda j : op.AND(j.pt > 20., op.abs(j.eta) < 2.4, j.jetId & 2))
     cleanedJets = op.select(jets, lambda j : op.AND(op.NOT(op.rng_any(electrons, lambda ele : op.deltaR(j.p4, ele.p4) < 0.3 )), op.NOT(op.rng_any(muons, lambda mu : op.deltaR(j.p4, mu.p4) < 0.3 ))))
     plots.append(Plot.make1D("nCleanedJets", op.rng_len(cleanedJets), noSel, EquidistantBinning(10, 0., 10.), title="Number of cleaned jets", xTitle="N_{j}"))
-    plots.append(Plot.make1D("btagSF", op.rng_product(cleanedJets, lambda j : j.btagSF), noSel, EquidistantBinning(100, 0., 1.2), title="Product of cleaned jets b-tag SF", xTitle="prod(btagSF)"))
-    plots.append(Plot.make1D("btagSF_shape", op.rng_product(cleanedJets, lambda j : j.btagSF_shape), noSel, EquidistantBinning(100, 0., 1.2), title="Product of cleaned jets b-tag shape SF", xTitle="prod(btagSF_shape)"))
+    if isMC:
+        plots.append(Plot.make1D("btagSF", op.rng_product(cleanedJets, lambda j : j.btagSF), noSel, EquidistantBinning(100, 0., 1.2), title="Product of cleaned jets b-tag SF", xTitle="prod(btagSF)"))
+        plots.append(Plot.make1D("btagSF_shape", op.rng_product(cleanedJets, lambda j : j.btagSF_shape), noSel, EquidistantBinning(100, 0., 1.2), title="Product of cleaned jets b-tag shape SF", xTitle="prod(btagSF_shape)"))
     cleanedJetsByDeepFlav = op.sort(cleanedJets, lambda jet: jet.btagDeepFlavB)
     hasMuJ = hasMuon.refine("hasMuonJ", cut=(op.rng_len(cleanedJets) > 0), weight=op.rng_product(cleanedJetsByDeepFlav, lambda jet: jet.btagDeepB))
     plots.append(Plot.make1D("hasMuonJ_prodBTags", op.rng_product(cleanedJetsByDeepFlav, lambda jet: jet.btagDeepB), hasMuJ, EquidistantBinning(1, 0., 1.), title="Product of jet b-tags", xTitle="X"))
     plots.append(Plot.make1D("MET", tup.MET.pt, noSel, EquidistantBinning(50, 0., 100.), title="MET pt", xTitle="MET"))
     plots.append(Plot.make1D("hasMuonJ_MET", tup.MET.pt, hasMuJ, EquidistantBinning(50, 0., 100.), title="MET pt", xTitle="MET"))
-    addsyst5p = op.systematic(op.c_float(1.), name="addSyst", up=op.c_float(1.05), down=op.c_float(0.95))
-    hasTwoJets = noSel.refine("hasTwoJets", cut=(op.rng_len(cleanedJets) > 1), weight=addsyst5p)
-    plots.append(Plot.make1D("2J_cleanedProdBRegCorr", op.rng_product(cleanedJetsByDeepFlav[:2], lambda j : j.bRegCorr), hasTwoJets, EquidistantBinning(50, 0.5, 1.5)))
+    if isMC:
+        addsyst5p = op.systematic(op.c_float(1.), name="addSyst", up=op.c_float(1.05), down=op.c_float(0.95))
+        hasTwoJets = noSel.refine("hasTwoJets", cut=(op.rng_len(cleanedJets) > 1), weight=addsyst5p)
+        plots.append(Plot.make1D("2J_cleanedProdBRegCorr", op.rng_product(cleanedJetsByDeepFlav[:2], lambda j : j.bRegCorr), hasTwoJets, EquidistantBinning(50, 0.5, 1.5)))
+    return plots
 
-    assert all(h for p in plots for h in be.getPlotResults(p))
+def testMC16(decoNanoMC16):
+    tup, noSel, be = decoNanoMC16
+    noSel = noSel.refine("mcWeight", weight=[ tup.genWeight, tup.puWeight ])
+    plots = definePlots(tup, noSel, isMC=True)
+    histos_per_plot = { p.name : list(be.getPlotResults(p)) for p in plots }
+    logger.debug("Plots for MC16")
+    for pN, histos in histos_per_plot.items():
+        logger.debug("Plot {0} -> {1}".format(pN, ", ".join(h.GetName() for h in histos)))
+    assert all(h for p,histos in histos_per_plot.items() for h in histos)
+
+def testData16(decoNanoData16):
+    tup, noSel, be = decoNanoData16
+    plots = definePlots(tup, noSel)
+    histos_per_plot = { p.name : list(be.getPlotResults(p)) for p in plots }
+    logger.debug("Plots for data16")
+    for pN, histos in histos_per_plot.items():
+        logger.debug("Plot {0} -> {1}".format(pN, ", ".join(h.GetName() for h in histos)))
+    assert all(len(histos) == 1 for histos in histos_per_plot.values())
+    assert all(h for p,histos in histos_per_plot.items() for h in histos)

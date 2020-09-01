7a81431f181b5e21ff54881198764a314aca7e81
Pieter David <pieter.david@cern.ch>
2020-09-01 16:50:43 +0200

    Fix return type detection for member function templates

--- bamboo/treeoperations.py
+++ bamboo/treeoperations.py
@@ -395,7 +395,7 @@ def guessReturnType(mp):
     oneDecl = None
     if hasattr(mp, "func_doc") and hasattr(mp, "func_name"):
         oneDecl = mp.func_doc.split("\n")[0] # overloads should have the same return type
-    elif hasattr(gbl, "TemplateProxy") and isinstance(mp, gbl.TemplateProxy):
+    elif ( hasattr(gbl, "TemplateProxy") and isinstance(mp, gbl.TemplateProxy) ) or (type(mp).__name__ == "TemplateProxy"):
         oneDecl = mp.__doc__.split("\n")[0]
     if oneDecl:
         toks = list(oneDecl.split())

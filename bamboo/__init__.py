81be14709f59c065337ba6ead8a923419385d6fe
Pieter David <pieter.david@cern.ch>
2021-05-28 16:17:49 +0200

    Remove copied bamboo source code

--- bamboo/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Bamboo
-
-A high-level HEP analysis library for ROOT::RDataFrame
-
-See https://gitlab.cern.ch/cp3-cms/bamboo
-"""
-import pkg_resources
-__version__ = pkg_resources.require("bamboo")[0].version
--- ext/include/BinnedValuesJSONParser.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class BinnedValues;
-
-namespace BinnedValuesJSONParser {
-    BinnedValues parse_file(const std::string& file);
-};
